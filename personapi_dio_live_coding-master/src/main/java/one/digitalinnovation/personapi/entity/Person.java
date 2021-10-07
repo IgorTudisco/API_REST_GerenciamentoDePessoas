24c9d6b1753f7f74cb97c62d2da94722a87cabc9
Igor Tudisco <igortudisco@hotmail.com>
2021-10-07 11:39:23 -0300

    Test

---
+++ personapi_dio_live_coding-master/src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -0,0 +1,49 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // gera o id de forma incremental.
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true) // valor único e não pode ser nulo.
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    /*
+    FetchType.LAZY indica que a class pessoa não precisa esperar o carregamento do phone para ser mostrado.
+    CascadeType.PERSIST indica que quando eu salvar o cliente o seu número também vai ser salvo junto.
+    CascadeType.REMOVE indica que quando eu remover o cliente o seu número também vai ser removido.
+     */
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Phone> phones;
+}
